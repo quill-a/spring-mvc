45c446c3dcb537b953e75ca9bbb14f9a16aeffae
quill-a <gabesander@gmail.com>
2025-08-07 21:06:55 -0500

    Add querying by ID

--- src/main/java/dev/quilla/springmvc/AlienDao.java
+++ src/main/java/dev/quilla/springmvc/AlienDao.java
@@ -1,6 +1,7 @@
 package dev.quilla.springmvc;
 
 import dev.quilla.springmvc.model.Alien;
+import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,26 @@ public class AlienDao {
     @Autowired
     private SessionFactory sessionFactory;
 
+    @Transactional
     public List<Alien> getAliens() {
         Session session = sessionFactory.getCurrentSession();
         List<Alien> aliens = session.createQuery("from Alien", Alien.class).list();
 
         return aliens;
+    }
+
+    @Transactional
+    public void addAlien(Alien alien) {
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(alien);
+    }
+
+    @Transactional
+    public Alien getAlien(int id) {
+
+        Session session = sessionFactory.getCurrentSession();
+        Alien alien = session.find(Alien.class, id);
+
+        return alien;
     }
 }
--- src/main/java/dev/quilla/springmvc/HomeController.java
+++ src/main/java/dev/quilla/springmvc/HomeController.java
@@ -4,10 +4,7 @@ import dev.quilla.springmvc.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -50,6 +47,16 @@ public class HomeController {
     @PostMapping("addAlien")
     public String addAlien(@ModelAttribute("alien1") Alien alien) {
 
+        dao.addAlien(alien);
+
         return "result";
+    }
+
+    @PostMapping("getAlien")
+    public String getAlien(@RequestParam int id,  Model model) {
+
+        model.addAttribute("result", dao.getAlien(id));
+
+        return "showAlien";
     }
 }
